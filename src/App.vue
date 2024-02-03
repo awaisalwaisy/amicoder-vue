6ea8847512a08af8c7b485df134f14a965fc15dd
Awais alwaisy <awaisalwaisy@gmail.com>
2024-02-03 09:31:26 +0500

    feat(app): add taiwind & daisy UI

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main class="app">Hello World</main>
+</template>
+
+<style scoped>
+.app {
+  @apply h-screen flex justify-center items-center text-4xl;
+}
+</style>
